df9ef582694098f799f937fd958e4256850f63b4
Simon "Tenry" Burchert <contact@simon-burchert.com>
2023-05-29 23:53:27 +0200

    Fix index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,30 @@
-declare module 'obsure-id' {
-  interface ObscureIdOptions {
-    key?: string;
-    index?: string;
-    defaultIdLength?: number;
-    prefixLength?: number;
-    randomFunction?: () => Promise<number>;
-  }
-
-  function obscureId(id: number, length?: number, random?: number[] | (() => Promise<number>)): Promise<string>;
-  function obscureId(id: string): Promise<number>;
-
-  namespace obscureId {
-    export function configure(options: ObscureIdOptions);
-    export function resetConfiguration();
-    export function maxId(length?: number): number;
-
-    export class ObscuredIdGenerator {
-      constructor(options?: ObscureIdOptions);
-      configure(options: ObscureIdOptions): this;
-      resetConfiguration(): this;
-      maxId(length?: number): number;
-      obscureId(id: number, length?: number, random?: number[] | (() => Promise<number>)): Promise<string>;
-      generate(id: number, length?: number, random?: number[] | (() => Promise<number>)): Promise<string>;
-      encode(id: number, length?: number, random?: number[] | (() => Promise<number>)): Promise<string>;
-      obscureId(id: string): Promise<number>;
-      decode(id: string): Promise<number>;
-    }
-  }
-
-  export = obscureId;
+interface ObscureIdOptions {
+  key?: string;
+  charset?: string;
+  defaultIdLength?: number;
+  prefixLength?: number;
+  randomFunction?: () => Promise<number>;
 }
+
+function obscureId(id: number, length?: number, random?: number[] | (() => Promise<number>)): Promise<string>;
+function obscureId(id: string): Promise<number>;
+
+namespace obscureId {
+  export function configure(options: ObscureIdOptions): void;
+  export function resetConfiguration(): void;
+  export function maxId(length?: number): number;
+
+  export class ObscuredIdGenerator {
+    constructor(options?: ObscureIdOptions);
+    configure(options: ObscureIdOptions): this;
+    resetConfiguration(): this;
+    maxId(length?: number): number;
+    obscureId(id: number, length?: number, random?: number[] | (() => Promise<number>)): Promise<string>;
+    generate(id: number, length?: number, random?: number[] | (() => Promise<number>)): Promise<string>;
+    encode(id: number, length?: number, random?: number[] | (() => Promise<number>)): Promise<string>;
+    obscureId(id: string): Promise<number>;
+    decode(id: string): Promise<number>;
+  }
+}
+
+export = obscureId;
